5489361b883a2a6a08791bb8e1a6ec35c095870c
Enner Godoy <ennergodoy@gmail.com>
2025-05-16 15:52:04 -0600

    Todo, excepto el configuracion de RabbitMQ

---
+++ turnos-handler/src/main/java/umg/edu/gt/turnos_handler/TurnosHandlerApplication.java
@@ -0,0 +1,13 @@
+package umg.edu.gt.turnos_handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnosHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnosHandlerApplication.class, args);
+	}
+
+}
---
+++ turnos-handler/src/test/java/umg/edu/gt/turnos_handler/TurnosHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package umg.edu.gt.turnos_handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnosHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
